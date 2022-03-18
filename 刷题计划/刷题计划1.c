1fb325437d9a23d056daa4aae811e85bb79fe066
39shanjiu <1918251321@qq.com>
2022-03-18 22:20:17 +0800

    第二天 第二题

--- 刷题计划/刷题计划1.c
+++ 刷题计划/刷题计划1.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+//void judy(int n, int k)
+//{
+//	int i = 0;
+//	for (n = 10000; n <= 30000; n++)
+//	{
+//		int sub1 = n / 100;
+//		int sub2 = (n / 10) % 1000;
+//		int sub3 = n % 1000;
+//		int a1 = sub1 % k;
+//		int a2 = sub2 % k;
+//		int a3 = sub3 % k;
+//
+//		if (a1 == a2 && a2 == a3 && a3 == 0)
+//		{
+//			i++;
+//			printf("%d\n", n);
+//		}
+//
+//	}
+//	if (i == 0)
+//	{
+//		printf("%s\n", "No");
+//	}
+//}
+//int main()
+//{
+//	int k = 0;
+//	do
+//	{
+//		scanf_s("%d", &k);
+//
+//	} while (k > 0 && k < 1000 ? 0 : 1);
+//	int n = 10000;
+//	judy(n, k);
+//	return 0;
+//}
 //void judy(int arr[], int N)
 //{
 	/*int i = 0;
